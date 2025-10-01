5b1c5f4500f398aaef0db3a879dc51812d9c890c
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-10-01 14:52:36 +0530

    create Assignment_6

---
+++ src/assignments/Assignment_6.jsx
@@ -0,0 +1,105 @@
+import "./Assignment_6.css";
+import React, { useState } from "react";
+import { styled } from "@mui/material";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+function Assignment_6() {
+  const [style, setStyle] = useState([]);
+  const [ruleName, setRuleName] = useState("");
+  const [ruleValue, setRuleValue] = useState("");
+
+  const ColorButton = styled(Button)({
+    backgroundColor: "#0E2148",
+    "&:hover": {
+      backgroundColor: "#1f5b8dff",
+    },
+  });
+
+  const DeleteButton = styled(Button)({
+    backgroundColor: "#8787872b",
+    "&:hover": {
+      backgroundColor: "#3d4c58ff",
+    },
+  });
+
+  const handleAdd = () => {
+    if (ruleName.trim() !== "" && ruleValue.trim() !== "") {
+      setStyle([...style, { name: ruleName, value: ruleValue }]);
+      setRuleName("");
+      setRuleValue("");
+    }
+  };
+
+  const handleDelete = (indexToRemove) => {
+    setStyle(style.filter((_, index) => index !== indexToRemove));
+  };
+
+
+  const cssObject = style.reduce(
+    (obj, item) => ({ ...obj, [item.name]: item.value }),
+    {}
+  );
+
+  return (
+    <div className="main asg-6">
+      <h1>Assignment #6</h1>
+
+      <div className="box">
+
+        
+
+        <Stack direction="row" spacing={2} alignItems="center" marginBottom={2}>
+          
+          <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Box>
+      <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Box>
+
+          <ColorButton onClick={handleAdd} variant="contained">
+            Add
+          </ColorButton>
+        </Stack>
+
+      
+        <ol className="list">
+          {style.map((rule, index) => (
+            <li key={index}>
+              <span>
+                {rule.name}: {rule.value}
+              </span>
+              <DeleteButton
+                variant="outlined"
+                size="small"
+                onClick={() => handleDelete(index)}
+              >
+                Delete
+              </DeleteButton>
+            </li>
+          ))}
+        </ol>
+
+        
+        <div className="sample-text" style={cssObject}>
+          Sample Text
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Assignment_6;
