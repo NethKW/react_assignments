4047744c9f05b8cf02c31c1504d9e215e73ed660
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-10-02 19:39:45 +0530

    update Assignment_8

--- src/assignments/Assignment_8.jsx
+++ src/assignments/Assignment_8.jsx
@@ -3,6 +3,35 @@ import React, { useState, /*useEffect*/ } from "react";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import { styled } from "@mui/material";
+import TextField from '@mui/material/TextField';
+
+const CssTextField = styled(TextField)({
+  '& label.Mui-focused': {
+    color: '#020e1aff',
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: '#020e1aff',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#020e1aff',
+    },
+    '&:hover fieldset': {
+      borderColor: '#020e1aff',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: '#020e1aff',
+    },
+  },
+});
+
+const ColorButton = styled(Button)({
+  backgroundColor: "#0E2148",
+  "&:hover": {
+    backgroundColor: "#1f5b8dff",
+  },
+});
 
 function Assignment_8() {
   const [colors, setColors] = useState([]);
@@ -34,17 +63,26 @@ function Assignment_8() {
   return (
     <div className="main asg-8">
       <h1>Assignment #8</h1>
-
       <div className="search-box">
-        <input
-          type="text"
-          placeholder="Enter color name..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <Button variant="contained" color="primary" onClick={handleSearch}>
-          Search
-        </Button>
+        <div >
+
+          <Box
+            component="form"
+            sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+            noValidate
+            autoComplete="off"
+          >
+            <CssTextField id="css_value" label="Color name" variant="outlined"
+              placeholder="Enter CSS value"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)} />
+          </Box>
+        </div>
+        <div>
+          <ColorButton variant="contained" color="primary" onClick={handleSearch}>
+            Search
+          </ColorButton>
+        </div>
       </div>
 
       <div className="box">
