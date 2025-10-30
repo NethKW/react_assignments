b657d9b64a04de8b4588258457fad85710ebef81
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-10-31 00:15:05 +0530

    update with game over

--- src/assignments/Assignment_27.jsx
+++ src/assignments/Assignment_27.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Assignment_27.css';
+import foodImg from '../assets/greenApple.png';
 
 const rows = 12;
 const cols = 12;
@@ -9,8 +10,7 @@ const mat = Array(rows)
   .map(() =>
     Array(cols).fill(0));
 
-
-function Sample() {
+function Assignment_27() {
   const [snake, setSnake] = useState([
     { x: 4, y: 4 },
     { x: 4, y: 3 },
@@ -18,14 +18,16 @@ function Sample() {
   ]);
 
   const [direction, setDirection] = useState("RIGHT");
+  const [food, setFood] = useState({ x: 6, y: 6 });
+  const [gameOver, setGameOver] = useState(false);
 
   useEffect(() => {
     const handleKeyDown = (e) => {
       const key = e.key.toLowerCase();
-      if ((key === "arrowup" || key==='w') && direction !== "DOWN") setDirection("UP");
-      else if ((key === "arrowdown" || key==='s') && direction !== "UP") setDirection("DOWN");
-      else if ((key === "arrowleft" || key==='a') && direction !== "RIGHT") setDirection("LEFT");
-      else if ((key === "arrowright" || key==='d') && direction !== "LEFT") setDirection("RIGHT");
+      if ((key === "arrowup" || key === 'w') && direction !== "DOWN") setDirection("UP");
+      else if ((key === "arrowdown" || key === 's') && direction !== "UP") setDirection("DOWN");
+      else if ((key === "arrowleft" || key === 'a') && direction !== "RIGHT") setDirection("LEFT");
+      else if ((key === "arrowright" || key === 'd') && direction !== "LEFT") setDirection("RIGHT");
     };
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
@@ -42,8 +44,20 @@ function Sample() {
       if (direction === "LEFT") head.y = (head.y - 1 + cols) % cols;
       if (direction === "RIGHT") head.y = (head.y + 1) % cols;
 
-      newSnake.unshift(head);
-      newSnake.pop();
+      if (newSnake.some(s => s.x === head.x && s.y === head.y)) {
+        setGameOver(true);
+        return;
+      }
+
+      let foodEat = head.x === food.x && head.y === food.y;
+      if (foodEat) {
+        newSnake.unshift(head);
+        generateFood(newSnake);
+      }
+      else {
+        newSnake.unshift(head);
+        newSnake.pop();
+      }
       setSnake(newSnake);
     };
     const interval = setInterval(() => {
@@ -51,11 +65,19 @@ function Sample() {
     }, 200);
 
     return () => clearInterval(interval);
-  }, [snake, direction]);
+  }, [snake, direction, food, gameOver]);
 
-
+  const generateFood = (currentSnake) => {
+    let x, y;
+    do {
+      x = Math.floor(Math.random() * rows);
+      y = Math.floor(Math.random() * cols);
+    } while (currentSnake.some((s) => s.x === x && s.y === y));
+    setFood({ x, y });
+  }
 
   const isSnakeCell = (x, y) => snake.some((s) => s.x === x && s.y === y);
+  const isFoodCell = (x, y) => food.x === x && food.y === y;
 
   return (
     <div className='asg-27 main'>
@@ -67,16 +89,23 @@ function Sample() {
             <div key={rIndex} className='row'>
               {row.map((cell, cIndex) => {
                 const snakeHere = isSnakeCell(rIndex, cIndex);
+                const foodHere = isFoodCell(rIndex, cIndex);
                 return (
-                  <div key={cIndex} className={`cell  ${snakeHere ? 'snake' : ''}`}></div>
+                  <div key={cIndex} className={`cell  ${snakeHere ? 'snake' : ''}`}>
+                    {foodHere && <img src={foodImg} className='food-img' />}
+                  </div>
                 )
               })}
             </div>
           ))}
         </div>
       </div>
+      {gameOver && (
+        <h2>Game Over!</h2>
+      )}
+
     </div>
   )
 }
 
-export default Sample
+export default Assignment_27
