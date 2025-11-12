67ab3c90d2c26af34d2dc5dbb303a8927e0ece22
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-11-12 23:20:05 +0530

    create Assignment_32

--- src/main.jsx
+++ src/main.jsx
@@ -31,6 +31,7 @@ import Assignment_28 from './assignments/Assignment_28.jsx';
 import Assignment_29 from './assignments/Assignment_29.jsx';
 import Assignment_30 from './assignments/Assignment_30.jsx';
 import Assignment_31 from './assignments/Assignment_31.jsx';
+import Assignment_32 from './assignments/Assignment_32.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -68,6 +69,7 @@ createRoot(document.getElementById('root')).render(
       <Route path="/ASG-29" element={<Assignment_29/>} />
       <Route path="/ASG-30" element={<Assignment_30/>} />
       <Route path="/ASG-31" element={<Assignment_31/>} />
+      <Route path="/ASG-32" element={<Assignment_32/>} />
     </Routes>
   </HashRouter>
 )
