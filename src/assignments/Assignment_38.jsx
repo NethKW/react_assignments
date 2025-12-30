c5f50a4b91a9cecca52015b67564c2c6b788ed78
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-30 21:30:08 +0530

    update water color of the bottle and bottle onclick changes

--- src/assignments/Assignment_38.jsx
+++ src/assignments/Assignment_38.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Assignment_38.css";
 
-export default function Assignment_38() {
+const bottleFill = [
+    ["blue", "red", "green"],
+    ["brown", "purple", "blue"],
+    ["green", "brown", "orange"],
+    ["blue", "purple", "brown"],
+    ["brown", "orange", "red"],
+    ["red", "green", "purple"],
+    ["blue", "orange", "purple"],
+    ["orange", "red", "green"]
+]
 
+export default function Assignment_38() {
+    const [bottleSelect,setBottleSelect] = useState(null);
+    const bottleClick = (i) => {
+        if (bottleSelect ===i) {
+            setBottleSelect(null)
+        } else {
+            setBottleSelect(i);
+        }
+    }
     return (
         <div className="main asg-38">
             <h2>Water Sort Puzzle</h2>
 
             <div className="puzzle">
                 <div className="bottles">
-                    {Array.from({ length: 8 }).map((_, i) => (
-                        <div className="bottle" key={i}>
-                            <div className="water red"></div>
-                            <div className="water blue"></div>
-                            <div className="water green"></div>
+                    {bottleFill.map((bottle, i) => (
+                        <div className={`bottle ${bottleSelect === i ? "selected" : ""}`} key={i} onClick={() => bottleClick(i)}>
+                            {bottle.map((color, index) => (
+                                <div
+                                    key={index}
+                                    className={`water ${color}`}
+                                ></div>
+                            ))}
                         </div>
                     ))}
                 </div>
