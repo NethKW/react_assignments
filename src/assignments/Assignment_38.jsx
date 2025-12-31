847690f13cda3223a9843acb2dce5144f7a48df4
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2025-12-31 15:35:10 +0530

    update with restart and complete game

--- src/assignments/Assignment_38.jsx
+++ src/assignments/Assignment_38.jsx
@@ -15,8 +15,10 @@ const bottleFill = [
 export default function Assignment_38() {
   const [bottle, setBottle] = useState(bottleFill); //game start display the bottleFill array color
   const [bottleSelect, setBottleSelect] = useState(null);
+  const [completed, setCompleted] = useState(false);
 
   const bottleClick = (i) => {
+    if (completed) return;
 
     if (bottleSelect === null) {
       setBottleSelect(i);
@@ -30,10 +32,22 @@ export default function Assignment_38() {
     setBottleSelect(null);
   }
 
+  const isGameCompleted = (bottles) => {
+    return bottles.every(
+      (b) =>
+        b.length === 0 || (b.length === 4 && b.every((color) => color === b[0]))
+    );
+  };
+
+  const restartGame = () => {
+    setBottle(bottleFill);
+    setBottleSelect(null);
+    setCompleted(false);
+  };
+
   //water filling according to same color 
   const waterFill = (source, target) => {
     const newBottleFill = bottle.map(b => [...b]);
-
     const source1 = newBottleFill[source];
     const target1 = newBottleFill[target];
 
@@ -47,15 +61,28 @@ export default function Assignment_38() {
     const sourceTop = source1[source1.length - 1];
     const targetTop = target1[target1.length - 1];
 
+    //check whether same color next to each other
     if (target1.length === 0 || sourceTop === targetTop) {
-      target1.push(source1.pop());
-      setBottle(newBottleFill);
-    }
+      let count = 1;
+      for (let i = source1.length - 2; i >= 0; i--) {
+        if (source1[i] === sourceTop) count++;
+        else break;
+      }
 
-    console.log("AFTER");
-    console.log("Source:", source1);
-    console.log("Target:", target1);
-  }
+      const space = 4 - target1.length;
+      const move = Math.min(count, space);
+
+      for (let i = 0; i < move; i++) {
+        target1.push(source1.pop());
+      }
+
+      setBottle(newBottleFill);
+
+      if (isGameCompleted(newBottleFill)) {
+        setCompleted(true);
+      }
+    }
+  };
   return (
     <div className="main asg-38">
       <h2>Water Sort Puzzle</h2>
@@ -73,6 +100,12 @@ export default function Assignment_38() {
             </div>
           ))}
         </div>
+        {completed && (
+        <div className="congrats-popup">
+          <h2>Congratulations!</h2>
+          <button onClick={restartGame}>Restart Game</button>
+        </div>
+      )}
       </div>
     </div>
   );
