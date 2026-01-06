a6496fecb353182c3498fa064957a6e522d11524
Nethmi Wijayasinghe <nethmiwijayasingha@gmail.com>
2026-01-06 09:10:51 +0530

    update the random car position with car moving

--- src/assignments-web-API/AssignmentW_08.jsx
+++ src/assignments-web-API/AssignmentW_08.jsx
@@ -17,10 +17,30 @@ function Vehicle({ sprite, style }) {
   )
 }
 
+const gameScreen = 500
+const roadLanes = [55, 120, 185, 250]
+
+const cars = [sprites[1], sprites[2], sprites[3], sprites[4], sprites[5]]
+
 export default function AssignmentW_08() {
 
   const roadScrolling = useRef(0)
   const [counter, setCounter] = useState(0)
+
+  const createCarPosition = () => {
+    let carPosition = []
+    let startY = -200
+    for (let i = 0; i < cars.length; i++) {
+      // Random lane and random gap
+      const lane = roadLanes[Math.floor(Math.random() * roadLanes.length)]
+      const gap = 400 + Math.random() * 150
+      carPosition.push({ sprite: cars[i], x: lane, y: startY })
+      startY -= gap
+    }
+    return carPosition
+  }
+
+  const carPositionRef = useRef(createCarPosition())
 
   useEffect(() => {
     let frame = null
@@ -32,6 +52,20 @@ export default function AssignmentW_08() {
 
       roadScrolling.current += 0.3 * delta //update scrolling value
       previousTime = time
+
+      carPositionRef.current = carPositionRef.current.map((enemy) => {
+        let newY = enemy.y + 0.3 * delta
+        let newX = enemy.x
+
+        if (newY > gameScreen) {
+          newX = roadLanes[Math.floor(Math.random() * roadLanes.length)]
+          newY = -(400 + Math.random() * 150)
+        }
+
+        return { ...enemy, y: newY, x: newX }
+      })
+
+
       setCounter(value => value + 1)
     }
     frame = requestAnimationFrame(update)
@@ -57,13 +91,13 @@ export default function AssignmentW_08() {
           }}
         />
 
-        <Vehicle
-          sprite={sprites[1]}
-          style={{
-            left: "160px",
-            top: "120px"
-          }}
-        />
+        {carPositionRef.current.map((enemy, i) => (
+          <Vehicle
+            key={i}
+            sprite={enemy.sprite}
+            style={{ left: `${enemy.x}px`, top: `${enemy.y}px` }}
+          />
+        ))}
 
         <div className="score">Score</div>
       </div>
